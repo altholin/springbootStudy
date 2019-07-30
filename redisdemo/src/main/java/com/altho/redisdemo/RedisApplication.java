9e255fed39a2eeaad58694e03e6797d8d6d713c5
Wanfang Li(联通上海市上海市分公司) <liwf75@chinaunicom.cn>
2019-07-30 15:04:19 +0800

    add redisdemo

---
+++ redisdemo/src/main/java/com/altho/redisdemo/RedisApplication.java
@@ -0,0 +1,26 @@
+// package com.altho.redisdemo;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class DemoApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(DemoApplication.class, args);
+// 	}
+
+// }
+package com.altho.redisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisApplication.class, args);
+	}
+}
+
